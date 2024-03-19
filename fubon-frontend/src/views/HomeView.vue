ba9b868512106fec23c96347b6aa5ed5f49db59a
geekender <gkndr716@gmail.com>
2024-03-19 23:04:40 +0800

    init

---
+++ fubon-frontend/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useCounterStore } from '@/stores/Pinia/counter'
+import { useStore } from '@/stores/Vuex/index';
+import { computed } from 'vue'
+
+const storePinia = useCounterStore();
+const count = storePinia.count;
+
+const storeVuex = useStore();
+const msg = computed(() => storeVuex.state.msg);
+</script>
+
+<template>
+    <main>
+        <!-- <TheWelcome /> -->
+        <h1>test pinia's count : {{ count }}</h1>
+        <h1>test Vuex's msg : {{ msg }}</h1>
+    </main>
+</template>
