8fb23f9d07e2165296fa5fa94ba2e75b8f1ce3cb
geekender <gkndr716@gmail.com>
2024-03-21 22:04:36 +0800

    test

---
+++ test-frontend/src/stores/Vuex/index.js
@@ -0,0 +1,32 @@
+// import Vue from 'vue'
+// import Vuex from 'vuex';
+
+// Vue.use(Vuex);
+
+// export default new Vuex.Store({
+//     state: {
+//         //存放值
+//         msg:"123"
+//     },
+//     getters: {
+//         //
+//     },
+//     mutations: {
+//         //
+//     },
+//     actions: {
+//         //呼叫mutations的入口，非同步操作在此完成
+//     },
+//     modules: {},
+// })
+
+// import { createStore } from "vuex";
+
+// const store = createStore({
+//     state: {
+//         //存放值
+//         msg: "123"
+//     }
+// })
+
+// export default store;
---
+++ test-frontend/src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { useCounterStore } from '@/stores/Pinia/counter'
+
+// import { useStore } from '';
+import { computed, onMounted } from 'vue'
+
+const storePinia = useCounterStore();
+const count = storePinia.count;
+
+// const storeVuex = useStore();
+// const msg = computed(() => storeVuex.state.msg);
+
+onMounted(() => {
+  const handleButtonClick = (evt) => {
+    //get target
+    const button = evt.target;
+    console.log("event touch", button)
+    console.log("按鈕被點擊了");
+
+
+  }
+})
+
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <h1>test pinia's count : {{ count }}</h1>
+    <!-- <h1>test Vuex's msg : {{ msg }}</h1> -->
+
+    <button id="myButton">Test</button>
+  </main>
+
+
+</template>
